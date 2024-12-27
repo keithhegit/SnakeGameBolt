23bacf960ba1c6a1e12e389d99d00c956c5e5e5d
Keith He <134305677+keithhegit@users.noreply.github.com>
2024-12-27 16:43:37 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
-    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY),
+  server: {
+    port: 3000,
+    open: true
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 });
